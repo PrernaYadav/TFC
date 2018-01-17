4763a148a296a0525ee7e9e267276e37c029e4d9
shreyansh <srivastav.lucky16@gmail.com>
2018-01-17 18:28:32 +0530

    second commit

--- app/src/main/java/com/infosolution/dev/tfc/activities/MainActivity.java
+++ app/src/main/java/com/infosolution/dev/tfc/activities/MainActivity.java
@@ -45,5 +45,14 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+        btnuserreg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,UserRegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/infosolution/dev/tfc/activities/ProductPageActivity.java
+++ app/src/main/java/com/infosolution/dev/tfc/activities/ProductPageActivity.java
@@ -2,14 +2,51 @@ package com.infosolution.dev.tfc.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.infosolution.dev.tfc.R;
 
 public class ProductPageActivity extends AppCompatActivity {
 
+    TextView proqty,tvminus,tvplus;
+    int count=1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_page);
+
+        proqty = (TextView) findViewById(R.id.tv_proqty);
+        tvminus = (TextView) findViewById(R.id.tv_minus);
+        tvplus = (TextView) findViewById(R.id.tv_plus);
+        tvminus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                count--;
+
+                if(count<0){
+                    count=0;
+                }
+
+
+
+                proqty.setText(String.valueOf(count));
+
+
+
+            }
+        });
+        tvplus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                count++;
+
+
+
+                proqty.setText(String.valueOf(count));
+
+            }
+        });
     }
 }
